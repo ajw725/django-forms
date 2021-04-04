6fd8e7f2cc248d13fdbdf920aab50d12a2a22206
Andrew Weinstein <andrew@altvia.com>
2021-04-03 21:41:43 -0600

    bot catcher - manual

--- forms_app/forms.py
+++ forms_app/forms.py
@@ -5,3 +5,9 @@ class FormName(forms.Form):
     name = forms.CharField()
     email = forms.EmailField()
     text = forms.CharField(widget=forms.Textarea)
+    bot_catcher = forms.CharField(required=False, widget=forms.HiddenInput)
+
+    def clean_bot_catcher(self):
+        bot_catcher = self.cleaned_data['bot_catcher']
+        if len(bot_catcher) > 0:
+            raise forms.ValidationError('Nice try, bot')
