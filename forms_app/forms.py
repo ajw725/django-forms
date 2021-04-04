3ebdb9ba79e441b2964acba0d6dff4190782007d
Andrew Weinstein <andrew@altvia.com>
2021-04-03 21:41:56 -0600

    bot catcher - build-in validator

--- forms_app/forms.py
+++ forms_app/forms.py
@@ -1,13 +1,11 @@
 from django import forms
+from django.core import validators
 
 
 class FormName(forms.Form):
     name = forms.CharField()
     email = forms.EmailField()
     text = forms.CharField(widget=forms.Textarea)
-    bot_catcher = forms.CharField(required=False, widget=forms.HiddenInput)
-
-    def clean_bot_catcher(self):
-        bot_catcher = self.cleaned_data['bot_catcher']
-        if len(bot_catcher) > 0:
-            raise forms.ValidationError('Nice try, bot')
+    bot_catcher = forms.CharField(required=False,
+                                  widget=forms.HiddenInput,
+                                  validators=[validators.MaxLengthValidator(0)])
